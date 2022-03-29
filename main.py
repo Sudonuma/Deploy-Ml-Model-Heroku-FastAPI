5c654ed9c7a151ea8b2a1f15df086b9b729edd23
Sudonuma <tasnim.hd@gmail.com>
2022-03-29 16:06:04 +0000

    change main

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ import os
 import sys
 import pandas as pd
 
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(os.path.dirname(SCRIPT_DIR))
+# SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+# sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 
 
