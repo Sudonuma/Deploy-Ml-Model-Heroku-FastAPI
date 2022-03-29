3873c4aae494bc5c65336aea7582fe66891e3c84
Sudonuma <tasnim.hd@gmail.com>
2022-03-29 20:10:23 +0000

    force dvc pull

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ sys.path.append(os.path.dirname(SCRIPT_DIR))
 
 if "DYNO" in os.environ and os.path.isdir(".dvc"):
     os.system("dvc config core.no_scm true")
-    if os.system("dvc pull") != 0:
+    if os.system("dvc pull --force") != 0:
         exit("dvc pull failed")
     os.system("rm -r .dvc .apt/usr/lib/dvc")
 
